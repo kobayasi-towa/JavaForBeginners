3c925d9cec42d4f3b1a5ec5169e1055e6d1edb12
kobayasi towa <zeal24360077@fsgcl.jp>
2025-06-03 10:55:53 +0900

    第5章、6章

---
+++ src/Animation.java
@@ -0,0 +1,19 @@
+
+public class Animation extends MyFrame{
+	public void run() {
+        int y=100;
+		
+		while (y<=200) {
+			clear();
+			
+			setColor(0,128,0);
+			fillOval(30,y,10,100); 
+			y +=5;
+			
+			
+			
+			sleep(0.1);
+		}
+	}
+
+}
